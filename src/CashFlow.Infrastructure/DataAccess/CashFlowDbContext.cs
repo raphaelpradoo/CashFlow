223c272d025c65e1fadc05b0082e6dc1363f9f94
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2026-03-09 23:06:09 -0300

    Conexão com o MySQL

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=@Rspo060469;";
+        var serverVersion = new MySqlServerVersion(new Version(8,0,45));
+
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
